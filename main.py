e9ea2d975cbf0c527fe70b4a890230567a9a2280
Irina Mogozova <12902@coderacademy.edu.au>
2022-07-10 16:43:41 +1000

    add parser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from image_processing import grayscale, rotate, resize
 from file_utils import list_image_files, is_image_file
+import argparse
 
 # grayscale("./test/picture_1.jpeg")
 #
@@ -7,9 +8,14 @@ from file_utils import list_image_files, is_image_file
 #
 # resize("./test/picture_1.jpeg", (500, 500))
 
-list_image_files("./test")
-print(list_image_files("./test"))
+# list_image_files("./test")
+# print(list_image_files("./test"))
+#
+# is_image_file("./test/picture_1.jpeg")
+# print(is_image_file("./test/picture_1.txt"))
 
-is_image_file("./test/picture_1.jpeg")
-print(is_image_file("./test/picture_1.txt"))
+parser = argparse.ArgumentParser(description="Photo Processor")
+parser.add_argument("--directory", help="directory help")
+args = parser.parse_args()
+print(args)
 
